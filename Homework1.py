76f4fd799c83deda67f757a63ddf184de090ccec
dareshesaidit <78746521+dareshesaidit@users.noreply.github.com>
2021-06-30 00:55:03 +0100

    Created branch3 using pycharm. Added notes from Git Bash practice

--- Homework1.py
+++ Homework1.py
@@ -1,1 +1,19 @@
-print('Hello World')
+print('Hiya World')
+print('My new line')
+
+"""
+Git Bash
+
+1. Right click to select Git Bash in the folder of choice
+2. A command terminal will load
+3. dir >> directory of folders
+4. cd #Folder_Name
+5. dir >> directory of items in folder
+
+Potential actions
+- git status #File_Name
+- git commit >> not completed
+- git push >> not shown
+- git branch #Branch_Name >> create a new branch >>not completed
+- git branch >> see most recent branch
+"""
